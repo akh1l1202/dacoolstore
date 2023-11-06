a0e0b90c2d952ede170f4372e5e93324fc69ace8
akh1l1202 <147427374+akh1l1202@users.noreply.github.com>
2023-11-06 18:15:31 +0530

    Update functions.py

--- merch store/functions.py
+++ merch store/functions.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 def connect_to_database():
     try:
         db = mysql.connector.connect(
-            host='sql12.freemysqlhosting.net',
+host='sql12.freemysqlhosting.net',
 user='sql12659263',
 password='FscMcuGwXr',
 database='sql12659263'
