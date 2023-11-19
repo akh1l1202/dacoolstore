7a8ea86e21d107556dbdbcd2422353e884e1662c
akh1l1202 <147427374+akh1l1202@users.noreply.github.com>
2023-11-19 11:50:40 +0530

    Update dacoolstore_categories.sql

--- database/dacoolstore_categories.sql
+++ database/dacoolstore_categories.sql
@@ -26,7 +26,7 @@ CREATE TABLE `categories` (
   `CategoryID` int NOT NULL,
   `CategoryName` varchar(255) NOT NULL,
   PRIMARY KEY (`CategoryID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
