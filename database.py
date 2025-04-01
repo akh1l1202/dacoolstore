822ee0834e50cc64cefb106fdc90fc660230497a
akhil <147427374+akh1l1202@users.noreply.github.com>
2025-04-01 11:15:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ def connect_to_database():
         db = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='######',
+            password='######',  #Replace the hashes with the paasword you setup for your local database server.
             database='dacoolstore',
         )
         return db
